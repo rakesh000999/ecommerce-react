3301e081abb7aad40c575460135870090a65a6ba
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-01-19 22:13:43 +0545

    [Add:] Search products

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import Shimmer from './Shimmer';
 
@@ -6,45 +6,69 @@ const Body = () => {
 
     const [listOfProducts, setListOfProducts] = useState([]);
     const [filteredProductList, setFilteredProductList] = useState([]);
-
+    const [searchText, setSearchText] = useState("");
     const [buttonName, setButtonName] = useState("Top Rated Products");
 
     useEffect(() => {
         fetchData();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (searchText === "") {
+            setFilteredProductList(listOfProducts);  // Reset filter if search text is empty
+        } else {
+            const filteredProducts = listOfProducts.filter((product) =>
+                product.title.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredProductList(filteredProducts);
+        }
+    }, [searchText, listOfProducts]);
 
     const fetchData = async () => {
         const data = await fetch('https://fakestoreapi.com/products');
         const json = await data.json();
-
         setListOfProducts(json);
         setFilteredProductList(json);
-    }
+    };
+
+    const filterProduct = () => {
+        if (buttonName === "Top Rated Products") {
+            setButtonName("All Products");
+            const filteredProductList = listOfProducts.filter((product) => product.rating.rate > 4);
+            setFilteredProductList(filteredProductList);
+        } else {
+            setButtonName("Top Rated Products");
+            setFilteredProductList(listOfProducts);
+        }
+    };
 
     return filteredProductList.length === 0 ? (
         <Shimmer />
     ) : (
         <div className='container'>
+            <div className='d-flex justify-content-between align-items-center'>
+                <div>
+                    <button
+                        className='btn btn-success my-3'
+                        onClick={filterProduct}
+                    >
+                        {buttonName}
+                    </button>
 
-            <button
-                className='btn btn-success my-3'
-                onClick={() => {
-                    if (buttonName == "Top Rated Products") {
-                        setButtonName("All Products");
-                        const filteredProductList = listOfProducts.filter(
-                            (product) =>
-                                product.rating.rate > 4
-                        )
-                        setFilteredProductList(filteredProductList);
-                    } else {
-                        setButtonName("Top Rated Products");
-                        const filteredProductList = setFilteredProductList(listOfProducts);
-                    }
-                }}
-            >
-                {buttonName}
-            </button>
+                    {/* Search Product */}
+                </div>
+                <div className='align-items-center'>
+                    <input
+                        type="text"
+                        className='form-control me-sm-2'
+                        value={searchText}
+                        placeholder='Seach product'
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
+                </div>
+            </div>
 
+            <hr />
             <div className="row">
                 {
                     filteredProductList.map((product) => (
@@ -52,8 +76,8 @@ const Body = () => {
                     ))
                 }
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
 export default Body;
